794786c4df4b60b81cc0500cb519eedd1f93bf47
Matthew Giovannini <shtyasek2003@mail.ru>
2022-08-02 22:14:10 +0500

    ListView class

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,15 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:  # если индекс больше, чем кол-во страниц
-        posts = paginator.page(paginator.num_pages)
-
-    context = {
-        'page': page,
-        'posts': posts
-    }
-    return render(request, template_name='blog/post/list.html', context=context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'post'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
